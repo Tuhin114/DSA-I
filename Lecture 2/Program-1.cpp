19452fc4f573319a875e0ee935c409f0841044c6
Tuhin114 <tuhinpoddar114@gmail.com>
2023-11-09 20:43:32 +0530

    Data types

--- Lecture 2/Program-1.cpp
+++ Lecture 2/Program-1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    // Your code here
-    cout << "Hello, world!" << endl;
+    int a = 123;
+    cout << a << endl;
+
+    char b = 'v';
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 4567890.123456789;
+    cout << d << endl;
+
+    int size = sizeof(d);
+    cout << "Size of variable: " << size << " bytes" << endl;
+
     return 0;
 }
