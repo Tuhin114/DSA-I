c5ac29a4a5581257d504b5297a8d38bbacc61d6a
Tuhin114 <tuhinpoddar114@gmail.com>
2023-11-09 21:25:46 +0530

    typecasting

--- Lecture 2/Program-1.cpp
+++ Lecture 2/Program-1.cpp
@@ -3,23 +3,29 @@ using namespace std;
 
 int main()
 {
-    int a = 123;
+    // int a = 123;
+    // cout << a << endl;
+
+    // char b = 'v';
+    // cout << b << endl;
+
+    // bool bl = true;
+    // cout << bl << endl;
+
+    // float f = 1.2;
+    // cout << f << endl;
+
+    // double d = 4567890.123456789;
+    // cout << d << endl;
+
+    // int size = sizeof(d);
+    // cout << "Size of variable: " << size << " bytes" << endl;
+
+    int a = 'a';
     cout << a << endl;
 
-    char b = 'v';
-    cout << b << endl;
-
-    bool bl = true;
-    cout << bl << endl;
-
-    float f = 1.2;
-    cout << f << endl;
-
-    double d = 4567890.123456789;
-    cout << d << endl;
-
-    int size = sizeof(d);
-    cout << "Size of variable: " << size << " bytes" << endl;
+    char ch = 98;
+    cout << ch << endl;
 
     return 0;
 }
