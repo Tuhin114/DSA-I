90e789e1e0e5b6112e7ca6854a045772486e7ff7
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-17 08:21:14 +0530

    36,37,38

--- Lecture 8/Program-36.cpp
+++ Lecture 8/Program-36.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 using namespace std;
 
+int power(int num1, int num2)
+{
+
+    int ans = 1;
+    for (int i = 0; i < num2; ++i)
+    {
+        ans *= num1;
+    }
+    return ans;
+}
+
 int main()
 {
+    int a, b;
+    cin >> a >> b;
+
+    int answer = power(a, b);
+    cout << "answer is " << answer << endl;
 }
--- Lecture 8/Program-37.cpp
+++ Lecture 8/Program-37.cpp
@@ -1,6 +1,33 @@
+// Even or Odd
 #include <iostream>
 using namespace std;
 
+// 1 -> Even
+// 0 -> odd
+bool isEven(int a)
+{
+    // odd
+    if (a & 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
 int main()
 {
+    int num;
+    cin >> num;
+
+    if (isEven(num))
+    {
+        cout << "even";
+    }
+    else
+    {
+        cout << "odd";
+    }
 }
