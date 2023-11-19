c8d3a87d833450a821508fd0f819e02f9aaf3d1b
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-19 07:37:23 +0530

    Aliter of 45

--- Lecture 9/Program 45.cpp
+++ Lecture 9/Program 45.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
 using namespace std;
 
-void reverse(int arr[], int n)
+// void reverse(int arr[], int n)
+// {
+
+//     int start = 0;
+//     int end = n - 1;
+
+//     while (start <= end)
+//     {
+//         swap(arr[start], arr[end]);
+//         start++;
+//         end--;
+//     }
+// }
+
+// void printArray(int arr[], int n)
+// {
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+// int main()
+// {
+//     int arrEven[6] = {1, 4, 0, 5, -2, 15};
+//     int arrOdd[5] = {2, 6, 3, 9, 4};
+
+//     reverse(arrEven, 6);
+//     reverse(arrOdd, 5);
+
+//     printArray(arrEven, 6);
+//     printArray(arrOdd, 6);
+
+//     return 0;
+// }
+
+// Alternative
+
+void swap(int arr[], int size)
 {
-
-    int start = 0;
-    int end = n - 1;
-
-    while (start <= end)
+    for (int i = 0; i < size - 1; i += 2)
     {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
+        if (i + 1 < size)
+        {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
     }
 }
 
 void printArray(int arr[], int n)
 {
-
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
+
 int main()
 {
     int arrEven[6] = {1, 4, 0, 5, -2, 15};
     int arrOdd[5] = {2, 6, 3, 9, 4};
 
-    reverse(arrEven, 6);
-    reverse(arrOdd, 5);
+    swap(arrEven, 6);
+    swap(arrOdd, 5);
 
     printArray(arrEven, 6);
-    printArray(arrOdd, 6);
+    printArray(arrOdd, 5);
 
     return 0;
 }
