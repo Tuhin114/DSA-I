858ddd7e79d5cf9df0898cc29735723fcd551f3b
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-19 10:22:33 +0530

    Lecture 10 CodeStudio

---
+++ Lecture 10/Program 46.cpp
@@ -0,0 +1,12 @@
+// CodeStudio - Find Unique
+// https://www.codingninjas.com/studio/problems/find-unique_625159?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1&leftPanelTabValue=PROBLEM
+
+// int findUnique(int *arr, int size)
+// {
+//     int ans = 0;
+
+//     for(int i = 0; i<size; i++){
+//         ans = ans^arr[i];  // XOR(^) cancels all same elements
+//     }
+//     return ans;
+// }
---
+++ Lecture 10/Program 47.cpp
@@ -0,0 +1,16 @@
+// CodeStudio
+// Duplicate In Array
+// https://www.codingninjas.com/studio/problems/duplicate-in-array_893397?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1&leftPanelTabValue=PROBLEM
+
+// int findDuplicate(vector<int> &arr)
+// {
+//     int ans = 0 ;
+//     for(int i = 0; i<arr.size();i++){
+//         ans = ans^arr[i];
+//     }
+//     for(int i = 1; i<arr.size();i++){
+//         ans = ans^i;
+//     }
+//     return ans;
+
+// }
