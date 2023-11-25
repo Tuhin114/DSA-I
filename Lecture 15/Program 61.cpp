41484ca18d6ab94885370324c4debd4b6797c8f2
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-25 11:38:54 +0530

    61 Aggressive Cows

---
+++ Lecture 15/Program 61.cpp
@@ -0,0 +1,44 @@
+// CodeStudio - Aggressive Cows
+// https://www.codingninjas.com/studio/problems/aggressive-cows_1082559?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2&leftPanelTabValue=PROBLEM
+
+// bool isPossible(vector<int> &stalls, int k, int mid){
+//     int cowCount = 1;
+//     int lastPos = stalls[0];
+
+//     for(int i=0; i<stalls.size(); i++){
+
+//         if(stalls[i]-lastPos >= mid){
+//             cowCount++;
+//             if(cowCount==k){
+//                 return true;
+//             }
+//             lastPos = stalls[i];
+//         }
+//     }
+//     return false;
+// }
+
+// int aggressiveCows(vector<int> &stalls, int k)
+// {
+//     sort(stalls.begin(),stalls.end());
+//     int s = 0;
+//     int maxi = -1;
+//     for(int i=0; i<stalls.size(); i++){
+//         maxi = max(maxi,stalls[i]);
+//     }
+//     int e = maxi;
+//     int ans = -1;
+//     int mid = s + (e-s)/2;
+
+//     while(s<=e){
+//         if(isPossible(stalls,k,mid)){
+//             ans = mid;
+//             s = mid + 1;
+//         }
+//         else{
+//             e = mid - 1;
+//         }
+//         mid = s + (e-s)/2;
+//     }
+//     return ans;
+// }
