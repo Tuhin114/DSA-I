7118372551e6a2e267e653e022bd20387f012498
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-27 07:17:29 +0530

    More Optimized

--- Lecture 17/Program 63.cpp
+++ Lecture 17/Program 63.cpp
@@ -8,11 +8,15 @@
 // {
 //     for( int i = 1; i<n; i++){
 //         //For round 1 to n-1
+//         bool swapped = false;
 
 //         for(int j = 0; j<(n-i); j++){
 //             if(arr[j]>arr[j+1]){
 //                 swap(arr[j],arr[j+1]);
+//                 swapped = true;
 //             }
 //         }
+//         if(swapped == false)
+//         break;
 //     }
 // }
