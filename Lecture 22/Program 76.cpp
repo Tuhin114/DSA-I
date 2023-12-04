3497d8feb904ca4b0c72a1adc98fa3856b282b13
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-04 17:24:50 +0530

    CodeStudio -  Replace Spaces

---
+++ Lecture 22/Program 76.cpp
@@ -0,0 +1,18 @@
+// CodeStudio -  Replace Spaces
+// https://www.codingninjas.com/studio/problems/replace-spaces_1172172?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_5&leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+// string replaceSpaces(string &str){
+// 	string temp = "";
+//     for(int i=0; i<str.length(); i++){
+//         if(str[i] == ' '){
+//             temp.push_back('@');
+//             temp.push_back('4');
+//             temp.push_back('0');
+//         }
+//         else{
+//             temp.push_back(str[i]);
+//         }
+//     }
+//     return temp;
+// }
