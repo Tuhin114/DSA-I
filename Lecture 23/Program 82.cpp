b592cf8e75ea55cecc45c3f0d536004bdcff8412
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-08 07:09:05 +0530

    LeetCode 74. Search a 2D Matrix

---
+++ Lecture 23/Program 82.cpp
@@ -0,0 +1,37 @@
+// LeetCode 74. Search a 2D Matrix
+//  You are given an m x n integer matrix matrix with the following two properties:
+//  Each row is sorted in non-decreasing order.
+//  The first integer of each row is greater than the last integer of the previous row.
+//  Given an integer target, return true if target is in matrix or false otherwise.
+//  You must write a solution in O(log(m * n)) time complexity.
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+//         int row = matrix.size();
+//         int col = matrix[0].size();
+
+//         int start = 0;
+//         int end = row*col-1;
+
+//         int mid = start+(end-start)/2;
+
+//         while(start<=end){
+
+//             int element = matrix[mid/col][mid%col];
+
+//             if(element == target){
+//                 return 1;
+//             }
+//             if(element > target){
+//                 end = mid-1;
+//             }
+//             else{
+//                 start = mid+1;
+//             }
+//             mid = start+(end-start)/2;
+//         }
+//         return 0;
+//     }
+// };
