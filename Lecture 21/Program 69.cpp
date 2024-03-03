805877f868303e0ae08a7c01bf866f9789474b51
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-03-03 10:15:43 +0530

    another solution

--- Lecture 21/Program 69.cpp
+++ Lecture 21/Program 69.cpp
@@ -22,3 +22,24 @@
 //         return count <= 1;
 //     }
 // };
+
+// Another Solution
+/**
+ class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int rotation = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] > nums[(i + 1) % n]){
+                rotation++;
+            }
+            if(rotation > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+*/
